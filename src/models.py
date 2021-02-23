4ebf1626c3b6fc49de8142ea3d67264cb132ff92
lucydoja <lucydoja@hotmail.com>
2021-02-23 16:55:27 +0000

    diagrama

--- src/models.py
+++ src/models.py
@@ -7,27 +7,49 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = "user"
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = "planets"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    population = Column(Integer)
+    terrain = Column(String(250))
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String(250))
+    url = Column(String(250), nullable=False)
+   
+class People(Base):
+    __tablename__ = "people"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    height = Column(Integer)
+    mass = Column(Integer)
+    hair_color = Column(String(250))
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+    birth_year = Column(Integer)
+    url = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Favorites(Base):
+    __tablename__ = "Favorites"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_name = Column(String(250), ForeignKey('planets.name'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    people_name = Column(String(250), ForeignKey('people.name'))
+    people_id = Column(Integer, ForeignKey('people.id'))
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
